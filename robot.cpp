ca0ad225c440a9cb123a2a7752aeaa43e19c90f8
kwsm114514 <88318477+kwsm114514@users.noreply.github.com>
2021-10-08 13:37:07 +0900

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -5,7 +5,7 @@ int main() {
 
     int robot_x_start = robot.get_x();
     int robot_y_start = robot.get_y();
-    printf("start x: %f, y: %f", robot_x_start, robot_y_start);
+    printf("start x: %d, y: %d", robot_x_start, robot_y_start);
 
     ROS_INFO("Left Sonar %i cm", ls);
     ROS_INFO("Right Sonar %i cm", rs);
@@ -48,7 +48,7 @@ int main() {
 
         // 動かせる
         robot.move(0.05, 0.0);
-        robot.wait();
+        robot.wait(0.5);
     } else if (ls != 0 && ls < 10) {
         robot.stop();
         robot.move(0.0, -1.0);
@@ -56,7 +56,7 @@ int main() {
     } else if (rs != 0 && rs < 10) {
         robot.stop();
         robot.move(0.0, 1.0);
-        robot.wait(0.5)
+        robot.wait(0.5);
     } else {
         robot.move(0.1, 0.0);
         robot.wait(0.5);
@@ -66,7 +66,7 @@ int main() {
     // debug
     int robot_x_end = robot.get_x();
     int robot_y_end = robot.get_y();
-    printf("start x: %f, y: %f", robot_x_end, robot_y_end);
+    printf("start x: %d, y: %d", robot_x_end, robot_y_end);
 }
 
 // cd catkin_wsb catkin_make
